b06b5f3b9c9c3cd3f98062258b8a8a72cd1b4e81
Frank <dirsky@126.com>
2019-09-16 18:16:49 +0800

    增加dir的参数/a:d

--- src/example/createFile.bat
+++ src/example/createFile.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 ::type nul>%%f\.gitkeep
 ::判断文件夹if exist %%f if exist %%f\nul
-for /f "delims="  %%f in ('dir /s /b') do (
+for /f "delims="  %%f in ('dir /s /b /a:d') do (
     ::注释的方案没有考虑空文件夹
     ::if exist %%f if exist %%f\nul type nul>%%f\.gitkeep
     set n=0
