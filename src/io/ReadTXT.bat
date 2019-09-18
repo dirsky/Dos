deda65c031cd7560a28d97d8416514946cc67bd7
Frank <dirsky@126.com>
2019-09-18 12:10:24 +0800

    修改及精简文件

--- src/io/ReadTXT.bat
+++ src/io/ReadTXT.bat
@@ -1,11 +1,12 @@
+@echo off
 ::循环读取txt
-::使用delims的目的主要是通过切分字符串获得可编辑的字符串以便于进行下一步编辑。
+::使用delims通过等于号后面的切分字符串获取前者部分
+
 ::样例数据
 ::ASSOC          显示或修改文件扩展名关联。
 ::ATTRIB         显示或更改文件属性。
 
-::/f的目的是指定in后面的是一个文件否则认为是一个字符串
-for /f "delims=" %%a in (HELP.txt) do (
+::/f指定in内是文件否则认为是字符串
+for /f "delims= " %%a in (HELP.txt) do (
     ECHO %%a
-    PAUSE
 )
